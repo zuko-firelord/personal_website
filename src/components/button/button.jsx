972869e694ec5e6cfa3a9e20cc2f95bb64015ac4
Sachin <tedwasachin123@gmail.com>
2022-09-26 23:55:56 +0530

    update project

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,8 +3,8 @@ import "./button.css";
 function Buttons() {
   return (
     <div className="container button-container">
-      <a href="#about" className="btn pri">
-        Learn more
+      <a href="https://github.com/TedwaSachin123" className="btn pri">
+        Github Profile
       </a>
       <a href="#contact" className="btn sec">
         Get in Touch
